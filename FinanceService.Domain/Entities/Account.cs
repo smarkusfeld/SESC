92f90fa1602c1954e391d8fa9e207807ea7053fe
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-06 23:42:55 +0100

    Added Domain Classes
    To Do: Set up other class libraries and tests

---
+++ FinanceService.Domain/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceService.Domain.Entities
+{
+    public class Account : IEntity
+    {
+        public string StudentID { get; set; }
+        public bool HasOutstandingBalance { get; set; }
+        public ICollection<Invoice>? Invoices { get; set; }
+        public ICollection<Payment>? Payments { get; set; }
+    }
+}
