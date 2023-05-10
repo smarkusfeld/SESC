eda9b1d808f78744c1a1bcecd5bfa3c4d88f9f70
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-10 13:45:25 +0100

    CleanSave before rebuilding project to fix dockerfile

--- FinanceService/FinanceService.API/Program.cs
+++ FinanceService/FinanceService.API/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 //add database if it does not exsit
 
@@ -22,7 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
