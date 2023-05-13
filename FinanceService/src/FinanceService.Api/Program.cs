7a80638a61e826356ad03e765beb51a048330262
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-13 16:15:03 +0100

    Initial Commit: Docker Containers Running
    All class library projects and api project set up
    Docker container and services running - 3 enabled services (1. db 2. api 3. adminer) in container
    MySql server is accessible from adminer on port 8080

---
+++ FinanceService/src/FinanceService.Api/Program.cs
@@ -0,0 +1,46 @@
+using FinanceService.Infastructure.Context;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("FinanceContext");
+builder.Services.AddDbContext<FinanceContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // add 10 seconds delay to ensure the db server is up to accept connections
+        // this won't be needed in real world application
+        System.Threading.Thread.Sleep(10000);
+        var context = services.GetRequiredService<FinanceContext>();
+        var created = context.Database.EnsureCreated();
+
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ FinanceService/src/FinanceService.Infastructure/Context/FinanceContext.cs
@@ -0,0 +1,17 @@
+﻿using FinanceService.Application.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceService.Infastructure.Context
+{
+    public class FinanceContext : DbContext
+    {
+        public FinanceContext(DbContextOptions<FinanceContext> options) : base(options) { }
+        public DbSet<Student>? Students { get; set; }
+    }
+}
