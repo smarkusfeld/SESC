ba9c13f4ffab49a0f843ac8e8793b9494d328591
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-14 12:24:42 +0100

    Updated Docker-compose and dockerfiles
    To Do: test data connection from api

---
+++ FinanceService/src/FinanceService.Application/DTOs/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceService.Application.DTOs
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
---
+++ FinanceService/src/FinanceService.Domain/Entities/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceService.Domain.Entities
+{
+    [Table("Invoices")]
+    public class Invoice : IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Reference { get; set; }
+        public decimal Total { get; set; }
+        public decimal Balance { get; set; }
+        public InvoiceType InvoiceType { get; set; }
+        public InvoiceStatus InvoiceStatus { get; set; }
+        public Account Account { get; set; }
+        public ICollection<Payment>? Payments { get; set; }
+    }
+    public enum InvoiceStatus
+    {
+        Outstanding,
+        Paid,
+        Candelled
+    }
+    public enum InvoiceType
+    {
+        Tutition,
+        Library
+    }
+}
