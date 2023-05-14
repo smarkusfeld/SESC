ca1c490fd23fb103d4f6912c8513fac45f0957be
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-14 16:16:14 +0100

    Successful connection from api to mysql in docker

---
+++ FinanceService/src/FinanceService.Api/Program.cs
@@ -0,0 +1,61 @@
+using FinanceService.Infastructure.Context;
+using FinanceService.Infastructure.Extensions;
+using Microsoft.OpenApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDIServices(builder.Configuration);
+//builder.Services.AddScoped<IAccountService, AccountService>();
+//builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+//builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "FinanceMicroservice",
+        Version = "v1",
+        Description = "todo",
+    });
+    // allow xml comments to be seen on swagger UI
+    //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // add 10 seconds delay to ensure the db server is up to accept connections
+        System.Threading.Thread.Sleep(10000);
+        var context = services.GetRequiredService<FinanceContext>();
+        var created = context.Database.EnsureCreated();
+
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "FinanceMicroservice v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+app.Run();
