a3622d365cfca676ce436d68968c9ff60bf11586
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-05-22 13:34:33 +0100

    Added Basic Entity Classes and initial erd

---
+++ LibraryService/src/LibraryService.Domain/Entities/Account.cs
@@ -0,0 +1,21 @@
+﻿using LibraryService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceService.Domain.Entities
+{
+    [Table("Accounts")]
+    public class Account : IEntity
+    {    
+        public string StudentID { get; set; }
+
+        public int Pin { get; set; } = 000000;
+
+        public ICollection<Loan> Loans { get; set; }
+    }
+}
---
+++ LibraryService/src/LibraryService.Domain/Entities/Author.cs
@@ -0,0 +1,19 @@
+﻿using FinanceService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService.Domain.Entities
+{
+    [Table("Authors")]
+    public class Author : IEntity
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
