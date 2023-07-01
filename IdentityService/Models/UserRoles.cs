c3c5609c992e9892dfd2ce5330eb52bf18810b77
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-07-01 15:50:34 +0100

    Identity Service Created - ToDo Unit Tests

---
+++ IdentityService/Models/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityService.Models
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Faculty = "Faculty";
+        public const string Student = "Student";
+        public const string Registrar = "Registrar";
+        public const string Library = "Library";
+        public const string Finance = "Finance";
+    }
+}
