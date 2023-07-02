4fedb11f89814ac5f1754860b7b3612987816c90
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-07-02 15:31:19 +0100

    Fixed Invoice Service Dockerfile and cleaned up solution code. XML comments now in swagger UI

---
+++ Services/Finance/src/FinanceService.Api/Program.cs
@@ -0,0 +1,66 @@
+
+using FinanceService.Application.Interfaces;
+using FinanceService.Application.Services;
+using FinanceService.Infastructure.Context;
+using FinanceService.Infastructure.Extensions;
+using Microsoft.OpenApi.Models;
+using NLog;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDIServices(builder.Configuration);
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+
+//builder.Services.AddScoped<IPaymentService, PaymentService>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "FinanceMicroservice",
+        Version = "v1",
+    });
+   
+});
+
+
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // add 10 seconds delay to ensure the db server is up to accept connections
+        System.Threading.Thread.Sleep(10000);
+        var context = services.GetRequiredService<DataContext>();
+        var created = context.Database.EnsureCreated();
+
+    }
+    catch (Exception ex)
+    {
+        var log = services.GetRequiredService<ILogger<Program>>();
+        log.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "FinanceMicroservice v1");
+    options.RoutePrefix = string.Empty;
+});
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
