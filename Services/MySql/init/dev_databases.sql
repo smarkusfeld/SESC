9200905d63bb6560e5d28e13a269064676957eb0
smarkusfeld <118601688+smarkusfeld@users.noreply.github.com>
2023-07-10 16:37:42 +0100

    Fixed docker compose mysql connection issues

--- Services/MySql/init/dev_databases.sql
+++
@@ -1,9 +0,0 @@
--- create databases
-CREATE DATABASE IF NOT EXISTS librarydb;
-CREATE DATABASE IF NOT EXISTS financedb;
-CREATE DATABASE IF NOT EXISTS identitydb;
-CREATE DATABASE IF NOT EXISTS registrardb;
-
--- create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'tiaspbiqe2r';
-GRANT ALL ON *.* TO 'root'@'localhost';
